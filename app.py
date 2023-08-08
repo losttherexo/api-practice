bb26ef0badb612aaa44a2f855819ca4dedff5ba1
Andre Vargas Roo <a.vargas252@icloud.com>
2023-08-08 14:46:59 -0400

    first endpoint

--- app.py
+++ app.py
@@ -4,4 +4,23 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-print('hello')
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+class Store(Resource):
+    def get(self):
+        return stores, 200
+    
+api.add_resource(Store, '/stores')
+
+if __name__ == '__main__':
+    app.run(debug=True)
