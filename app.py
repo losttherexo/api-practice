fe0e98a0a2e7ab5e04261ecbd512e8746308f775
Andre Vargas Roo <a.vargas252@icloud.com>
2023-08-09 12:24:12 -0400

    update api

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+import uuid
 from flask import Flask, make_response, request
 from flask_restful import Resource, Api
-
-from db import stores, items
+from db import items, stores
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,61 +12,68 @@ class Home(Resource):
 
 class Store(Resource):
     def get(self):
-        response = make_response(stores, 200)
+        response = make_response(list(stores.values()), 200)
 
         return response
     
     def post(self):
         data = request.get_json()
+        id = uuid.uuid4().hex
 
-        new_store = {'name': data['name'], 'items': []}
+        new_store = {**data, "id": id}
 
-        stores.append(new_store)
+        stores[id] = new_store
         response = make_response(new_store, 201)
 
         return response
     
 class StoreByName(Resource):
-    def get(self, name):
-        for s in stores:
-            if s['name'] == name:
-                print(name)
-                response = make_response(s, 201)
+    def get(self, store_id):
+        try:
+            response = make_response(stores['store_id'], 200)
 
-                return response
-        response = make_response({'error': '404, Store not found'}, 404)
+            return response
+        except KeyError: 
+            response = make_response({'error': '404, Store not found'}, 404)
 
-        return response
+            return response
     
-class StoreItem(Resource):
-    def get(self, name):
-        for s in stores:
-            if s['name'] == name:
-                response = make_response(s['items'], 200)
-
-                return response
-        response = make_response({'error': '404, Store not found'}, 404)
+class Item(Resource):
+    def get(self):
+        response = make_response(list(items.values()), 200)
 
         return response
 
-    def post(self, name):
+    def post(self):
         data = request.get_json()
 
-        for s in stores:
-            if s['name'] == name:
-                new_item = {'name': data['name'], 'price': data['price']}
-                s['items'].append(new_item)
+        if data['store_id'] not in stores:
+            response = make_response({'error': '404, Store not found'}, 404)
 
-                response = make_response(new_item, 201)
-                return response
-        response = make_response({'error': '404, Store not found'}, 404)
+            return response
+
+        id = uuid.uuid4().hex
+        item = {**data, 'id': id}
+        response = make_response(item, 201)
 
         return response
     
+class ItemByName(Resource):
+    def get(self, item_id):
+        try:
+            response = make_response(items['item_id'], 200)
+
+            return response
+        except KeyError: 
+            response = make_response({'error': '404, Store not found'}, 404)
+
+            return response
+
 api.add_resource(Home, '/')
 api.add_resource(Store, '/store')
-api.add_resource(StoreByName, '/store/<string:name>')
-api.add_resource(StoreItem, '/store/<string:name>/item')
+api.add_resource(StoreByName, '/store/<string:store_id>')
+api.add_resource(Item, '/item')
+api.add_resource(ItemByName, '/item/<string:item_id>')
 
 if __name__ == '__main__':
     app.run()
